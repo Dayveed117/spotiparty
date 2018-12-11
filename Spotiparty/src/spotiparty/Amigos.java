abaa19538a0d6ecb810f74b31ca66ff119211342
Dayveed117 <david.bugalho@ubi.pt>
2018-12-11 18:58:25 +0000

    Adicionei informaçoes importantes2

--- Spotiparty/src/spotiparty/Amigos.java
+++ Spotiparty/src/spotiparty/Amigos.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projeto;
+package Spotiparty;
 import java.util.*;
 import javax.swing.JOptionPane;
 /**
