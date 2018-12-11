afee1c9845c73f690e34aba935bc7be3d60a01ab
Dayveed117 <david.bugalho@ubi.pt>
2018-12-11 21:52:14 +0000

    Musica e BibliotecaMusical
    lololoo

---
+++ Spotiparty/src/spotiparty/BibliotecaMusical.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spotiparty;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author a40284
+ */
+public class BibliotecaMusical {
+    
+    private ArrayList<Musica> Musicas;
+    
+    public BibliotecaMusical() {
+        Musicas = null;
+    }
+            
+    public ArrayList<Musica> getMusicas() {
+        return Musicas;
+    }
+
+    public void setMusicas(ArrayList<Musica> Musicas) {
+        this.Musicas = Musicas;
+    }
+    
+    public void adicionarMusica(Musica mus) {
+        if(Musicas.contains(mus)) {
+              JOptionPane.showMessageDialog(null, "Essa musica já esta na biblioteca!");
+        }
+        else {
+            Musicas.add(mus);
+            JOptionPane.showMessageDialog(null, "Música adicionada com sucesso");
+        }
+    }
+    
+    public void removerMusica(Musica mus) {
+        if(Musicas.contains(mus)) {
+            Musicas.remove(mus);
+            JOptionPane.showMessageDialog(null, "Musica removida da Biblioteca");
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Musica não encontrada");
+        }
+    }
+    
+    @Override
+    public String toString() {
+        String s = "";
+        for(Musica m: Musicas) {
+            s += m.toString();
+        }
+        return(s);
+    }
+    
+}
--- Spotiparty/src/spotiparty/Musica.java
+++ Spotiparty/src/spotiparty/Musica.java
@@ -5,17 +5,103 @@
  */
 package spotiparty;
 
+import java.util.Objects;
+
 /**
  *
  * @author a40284
  */
 public class Musica {
     
-    private int Nome;
-    private double duracao;
-    private String titulo, autor, genero;
+    private double duracao, rating;
+    private String titulo, autor, genero, album;
     
-    /*criar musica
-    remover musica
-    */
+    public Musica() {
+    }
+    
+    public Musica(double duracao, double rating, String titulo, String autor, String album, String genero) {
+       
+        this.duracao = duracao;
+        this.rating = rating;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.album = album;
+        this.genero = genero;
+        
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+    
+    public double getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(double duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if(obj != null && this.getClass() == obj.getClass()) {
+            Musica temp = (Musica)obj;
+            return ((album.equals(temp.getAlbum())) && (autor.equals(temp.getAutor())) && (this.getDuracao() == temp.getDuracao())
+                    && genero.equals(temp.genero) && this.getRating() == temp.getRating() && titulo.equals(temp.getTitulo()));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 71 * hash + Objects.hashCode(this.titulo);
+        hash = 71 * hash + Objects.hashCode(this.autor);
+        hash = 71 * hash + Objects.hashCode(this.genero);
+        hash = 71 * hash + Objects.hashCode(this.album);
+        return hash;
+    }
+    
+    @Override
+    public String toString() {
+        return("\nTitulo: "+titulo+"\nArtista: "+autor+"; Album: "+album+
+                "; Genero: " +genero+"\nDuração: "+duracao+"Rating: "+rating+"\n");
+    }
+    
 }
